002de00f270c7be1f04a58ade4c976aca8127d39
Devine Lu Linvega <aliceffekt@gmail.com>
2015-05-10 10:50:26 -0400

    Hollowed the interface

---
+++ nijuniju/nijuniju/Lesson.m
@@ -0,0 +1,13 @@
+//
+//  Lesson.m
+//  Vocavularist
+//
+//  Created by Devine Lu Linvega on 2015-05-10.
+//  Copyright (c) 2015 XXIIVV. All rights reserved.
+//
+
+#import "Lesson.h"
+
+@implementation Lesson
+
+@end
---
+++ nijuniju/nijuniju/Lessons.h
@@ -0,0 +1,13 @@
+//
+//  xxiivvLessons.h
+//  Vocavularist
+//
+//  Created by Devine Lu Linvega on 2015-05-10.
+//  Copyright (c) 2015 XXIIVV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lessons : NSObject
+
+@end
